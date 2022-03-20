b9b7ba0a571f3cd8fd1c7adeb24212ebd0bc8920
Redowanur <69495647+Redowanur@users.noreply.github.com>
2022-03-20 22:43:02 +0600

    Add files via upload

---
+++ Heap/mean_heap.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(int *arr, int sz){
+    for(int i = 1; i <= sz; i++) cout << arr[i] << " ";
+    cout << endl;
+}
+
+void max_heap(int *arr, int sz){
+    for(int i = 2; i <= sz; i++){
+        int j = i;
+        int parent = j/2;
+
+        while(arr[j] < arr[parent] && parent >= 1){
+            swap(arr[j], arr[parent]);
+            j = parent;
+            parent /= 2;
+        }
+    }
+}
+
+int main(){
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for(int i = 1; i <= n; i++){
+        cin >> arr[i];
+    }
+
+    int size = sizeof(arr)/sizeof(arr[1]);
+
+    max_heap(arr, size);
+
+    print(arr, size);
+    
+    return 0;
+}
